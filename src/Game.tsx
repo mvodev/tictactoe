c8df509cd81ee9bc9ac30e09b1d0e0bcb9011bf0
Vadim Manushin <vadimmanushin@yandex.ru>
2026-01-08 22:50:38 +0300

    added click handler on game field

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,16 @@
 
+import type { PointerEvent } from 'react';
 import styles from './Game.module.css';
 
 const Game = () => {
-
+  const handleClick = (event:PointerEvent<HTMLElement>)=>{
+    const eventTarget = event.target as HTMLDivElement 
+    console.log(eventTarget.id);
+  }
   return (
     <>
-      <h1>TicTacToe Game</h1>
-      <main className={styles.gameroot}>
+      <h1 className={styles.header}>TicTacToe Game</h1>
+      <main onPointerDown={handleClick} className={styles.gameroot}>
         <div className={styles.field} id='1-1'></div>
         <div className={styles.field} id='1-2'></div>
         <div className={styles.field} id='1-3'></div>
